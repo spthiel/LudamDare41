e6b7be67bb34c7836af1937290b5ded0d8a3bcf0
Unknown <steppi.thiel@googlemail.com>
2018-04-20 23:11:48 +0200

    Added stuff that might work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 
+var frame;
+var screen;
+
 function setup() {
 	screen.w = window.innerWidth;
 	screen.h = window.innerHeight;
 	let canvas = createCanvas(screen.w,screen.h);
-	document.getElementsByClassName("placeholder")[0].appendChild(canvas);
+	document.getElementsByClassName("placeholder")[0].appendChild(canvas.canvas);
+	frame = 0;
+}
+
+function getColor(frame) {
+	return "hsv(" + frame%360 + ")";
 }
 
 function draw() {
-
+	fill(100);
+	translate(screen.w/2,screen.h/2);
+	textAlign(CENTER,TOP);
+	text("Something big will be here (maybe)");
 }
+
+window.addEventListener( 'resize', function() {
+
+  setup();
+
+});
